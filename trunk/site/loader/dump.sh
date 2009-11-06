963b3737cf4b76b4cbc4a5449f7e2aa34372ab8a
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-06 09:17:42 +0000

    Added app_id for localhost restore/dump

--- trunk/site/loader/dump.sh
+++ trunk/site/loader/dump.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 APP_DIR=$1
 DATA_DIR=$2
+HOST=$3
 rm -rf $DATA_DIR
 mkdir $DATA_DIR
-bulkloader.py --dump --kind=Owner --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/owners $APP_DIR
-bulkloader.py --dump --kind=Venue --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/venues $APP_DIR
-bulkloader.py --dump --kind=PhoneNumber --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/number $APP_DIR
-bulkloader.py --dump --kind=Address --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/addresses $APP_DIR
-bulkloader.py --dump --kind=EmailAddress --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/emails $APP_DIR
-bulkloader.py --dump --kind=Inspection --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/inspections $APP_DIR
-bulkloader.py --dump --kind=Complaint --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/complaints $APP_DIR
-bulkloader.py --dump --kind=Bedroom --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/bedrooms $APP_DIR
-bulkloader.py --dump --kind=Bathroom --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/bathrooms $APP_DIR
-bulkloader.py --dump --kind=Bed --url=http://bookings-dev.appspot.com/unload --filename=$DATA_DIR/beds $APP_DIR
+bulkloader.py --dump --kind=Owner --url=$HOST/unload --filename=$DATA_DIR/owners $APP_DIR
+bulkloader.py --dump --kind=Venue --url=$HOST/unload --filename=$DATA_DIR/venues $APP_DIR
+bulkloader.py --dump --kind=PhoneNumber --url=$HOST/unload --filename=$DATA_DIR/number $APP_DIR
+bulkloader.py --dump --kind=Address --url=$HOST/unload --filename=$DATA_DIR/addresses $APP_DIR
+bulkloader.py --dump --kind=EmailAddress --url=$HOST/unload --filename=$DATA_DIR/emails $APP_DIR
+bulkloader.py --dump --kind=Inspection --url=$HOST/unload --filename=$DATA_DIR/inspections $APP_DIR
+bulkloader.py --dump --kind=Complaint --url=$HOST/unload --filename=$DATA_DIR/complaints $APP_DIR
+bulkloader.py --dump --kind=Bedroom --url=$HOST/unload --filename=$DATA_DIR/bedrooms $APP_DIR
+bulkloader.py --dump --kind=Bathroom --url=$HOST/unload --filename=$DATA_DIR/bathrooms $APP_DIR
+bulkloader.py --dump --kind=Bed --url=$HOST/unload --filename=$DATA_DIR/beds $APP_DIR
 rm -rf bulkloader-*
