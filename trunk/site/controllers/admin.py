532aff3ad36e7f6aa8d5157534b3245fc8f24b10
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-09 13:09:45 +0000

    Added matches

--- trunk/site/controllers/admin.py
+++ trunk/site/controllers/admin.py
@@ -1,23 +1,34 @@
-import cgi
 import os
-from google.appengine.api import users, images
+import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
-from google.appengine.ext import db
-import populate
 
-from model import ArtWork
+from controllers.home import BASE_PATH, PROJECT_PATH
+from controllers.match import CaptureMatch, EditMatch, DeleteMatch
+from controllers.utils import get_authentication_urls
+from models.schedule import Match
 
 
 class AdminHomePage(webapp.RequestHandler):
     def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'admin', 'admin.html')
-        self.response.out.write(template.render(path, {}))
+        auth_url, auth_url_text = get_authentication_urls(self.request.uri)
+        matches = Match.all().order('number')
+        filepath = os.path.join(PROJECT_PATH, 'templates', 'admin', 'adminhome.html')
+        self.response.out.write(template.render(filepath, 
+                                    {
+                                        'base_path':BASE_PATH,
+                                        'matches':matches,
+                                        'auth_url':auth_url,
+                                        'auth_url_text':auth_url_text
+                                        }))
 
 
 application = webapp.WSGIApplication([
                             ('/admin/home', AdminHomePage),
+                            ('/admin/schedule/capturematch', CaptureMatch),
+                            ('/admin/schedule/editmatch', EditMatch),
+                            ('/admin/schedule/deletematch', DeleteMatch),
                             ], debug=True)
 
 def main():
