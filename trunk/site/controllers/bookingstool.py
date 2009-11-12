861d566e28d1ad8ded2aeac63da715403643c0e3
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-12 14:00:20 +0000

    Changed bookings/slot models; got manual contractedbookings to work

--- trunk/site/controllers/bookingstool.py
+++ trunk/site/controllers/bookingstool.py
@@ -1,7 +1,13 @@
 import logging
+import urllib
 from datetime import datetime, timedelta
+
+from google.appengine.ext import webapp
 from google.appengine.ext.db import Query
+
 from models.hostinfo import Slot, Berth
+from models.bookinginfo import \
+    ContractedBooking, Enquiry, AccommodationElement
 
 logger = logging.getLogger('BookingsTool')
 
@@ -52,12 +58,12 @@ class BookingsTool():
         berths = {}
         for slot in slots:
             #logger.info("Found berth %s", slot.berth.key())
-            berthkey = slot.berth.key()
+            berthkey = str(slot.berth.key())
             if berths.has_key(berthkey):
-                berths[berthkey]['slots'].append(slot)
+                berths[berthkey]['slots'].append(str(slot.key()))
             else:
-                berths[berthkey] = {'slots': [slot], 'valid':False}
-        #check for complete and continuous pairings
+                berths[berthkey] = {'slots': [str(slot.key())], 'valid':False}
+        #check for complete 
         for key in berths.keys():
             #logger.info("Check pair %s", key)
             valid = True #until proven otherwise
@@ -65,18 +71,6 @@ class BookingsTool():
             if len(berths[key]['slots']) != nights:
                 logger.info("INVALID: Pairing for %s is incomplete", key)
                 break #it remains false
-            #Check continuous - not required
-            #current = None
-            #for slot in berths[key]['slots']:
-            #    #logger.info("Check %s against %s", slot.startDate, current)
-            #    if current is None:
-            #        current = slot.startDate
-            #    else:
-            #        if slot.startDate != current + timedelta(days = 1):
-            #            valid = False
-            #            break
-            #        else:
-            #            current = slot.startDate
             if valid:
                 berths[key]['valid'] = True
                 
@@ -92,4 +86,73 @@ class BookingsTool():
         return valid_berths
 
 
+class BookingsToolFindAccommodation(webapp.RequestHandler):
+    def post(self):
+        tool = BookingsTool()
+        came_from = '/bookings/bookinginfo' #self.request.referer
+        city = self.request.get('city')
+        type = self.request.get('type')
+        start = self.request.get('start')
+        if start:
+          start = datetime.strptime(start, '%Y-%m-%d').date()
+        nights = self.request.get('nights')
+        if nights:
+            nights = int(nights)
+        people = self.request.get('people')
+        if people:
+            people = int(people)
+        #Generate number
+        enquiry = Enquiry(referenceNumber='123')
+        enquiry.put()
+        accom_element = AccommodationElement(
+            enquiry=enquiry,
+            city=city,
+            type=type,
+            start=start,
+            nights=nights,
+            people=people)
+        accom_element.put()
+
+        berths = tool.findBerths(city, type, start, nights, people)
+        params = {}
+        if berths:
+            accom_element.available_berths = str(berths)
+            accom_element.put()
+            params['elementkey'] = accom_element.key() 
+        params = urllib.urlencode(params)
+        self.redirect("%s?%s" % (came_from, params))
+
+class BookingsToolReserveAccommodation(webapp.RequestHandler):
+    def post(self):
+        came_from = '/bookings/bookinginfo' #self.request.referer
+        args = self.request.arguments()
+        elementkey = self.request.get('elementkey') 
+        if elementkey:
+            element = AccommodationElement.get(elementkey)
+            enquiry=element.enquiry
+            if element.available_berths:
+                berthkeys = []
+                for arg in args:
+                    if arg.startswith('berth_'):
+                        berthkey = arg.split('_')[-1]
+                        berthkeys.append(berthkey)
+                #Create Booking
+                booking = ContractedBooking(
+                    bookingNumber='123',
+                    enquiry=enquiry)
+                booking.put()
+                #Mark slots as occupied
+                for berthkey, slotkeys in eval(element.available_berths):
+                    if berthkey in berthkeys:
+                        for slotkey in slotkeys:
+                            slot = Slot.get(slotkey) 
+                            slot.occupied = True
+                            slot.contracted_booking = booking
+                            slot.put()
+            #Clean up
+            if enquiry.state == 'temporary':
+                enquiry.rdelete()
+        params = {}
+        params = urllib.urlencode(params)
+        self.redirect("%s?%s" % (came_from, params))
 
--- trunk/site/models/bookinginfo.py
+++ trunk/site/models/bookinginfo.py
@@ -1,6 +1,5 @@
 from google.appengine.ext import db
 from models.clientinfo import Client
-from models.hostinfo import Slot
 
 class Enquiry(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
@@ -15,6 +14,10 @@ class Enquiry(db.Model):
         return '%s' % self.referenceNumber
 
     def rdelete(self):
+        for e in self.guest_elements:
+            e.rdelete()
+        for e in self.accommodation_elements:
+            e.rdelete()
         self.delete()
 
 class AccommodationElement(db.Model):
@@ -27,17 +30,18 @@ class AccommodationElement(db.Model):
         verbose_name='City',
         default='Potchefstroom',
         choices=['Potchefstroom', 'Cape Town'])
-    serviceType = db.StringProperty(
+    type = db.StringProperty(
         required=True, 
         verbose_name='Accommodation Class',
-        choices=['Backpackers', 'Guest House', 'Home Stay', 'Hostel'])
-    startDate = db.DateProperty(
+        choices=['Backpackers', 'Guest House', 'Family House', 'Hostel'])
+    start = db.DateProperty(
         required=True, verbose_name='Start Date')
-    duration = db.IntegerProperty(
+    nights = db.IntegerProperty(
         required=True, verbose_name='Number of nights')
-    quantity = db.IntegerProperty(
+    people = db.IntegerProperty(
         required=True, verbose_name='Number of people')
     xmlSource = db.TextProperty(verbose_name='Source Detail')
+    available_berths = db.TextProperty()
 
     def listing_name(self):
         return '%s' % self.city
@@ -58,14 +62,15 @@ class GuestElement(db.Model):
     identifyingNumber = db.StringProperty(verbose_name='Identifying Number')
     xmlSource = db.TextProperty(verbose_name='Source Detail')
         
+    def rdelete(self):
+        self.delete()
+
 class ContractedBooking(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     creator = db.UserProperty()
     bookingNumber = db.StringProperty(required=True)
     client = db.ReferenceProperty(
         Client, collection_name='contraced_bookings')
-    slot = db.ReferenceProperty(
-        Slot, collection_name='contracted_bookings')
     enquiry = db.ReferenceProperty(
         Enquiry, collection_name='contracted_bookings')
     state = db.StringProperty(
@@ -75,5 +80,8 @@ class ContractedBooking(db.Model):
         return '%s' % self.bookingNumber
 
     def rdelete(self):
+        for e in self.slots:
+            e.occupied = False
+            e.put()
         self.delete()
 
