f00f34961ccf946358003a5b293a2ebf8e8f8819
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-12 09:43:36 +0000

    Changed bookingrequest to enquiry; added models for equiry elements

--- site/models/bookinginfo.py
+++ site/models/bookinginfo.py
@@ -2,11 +2,26 @@ from google.appengine.ext import db
 from models.clientinfo import Client
 from models.hostinfo import Slot
 
-class BookingRequest(db.Model):
+class Enquiry(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     creator = db.UserProperty()
     referenceNumber = db.StringProperty(
         required=True, verbose_name='Reference Number')
+    state = db.StringProperty(
+        default='temporary', choices=['temporary', 'confirmed'])
+    xmlSource = db.TextProperty(verbose_name='Source Detail')
+
+    def listing_name(self):
+        return '%s' % self.referenceNumber
+
+    def rdelete(self):
+        self.delete()
+
+class AccommodationElement(db.Model):
+    created = db.DateTimeProperty(auto_now_add=True)
+    creator = db.UserProperty()
+    enquiry = db.ReferenceProperty(
+        Enquiry, collection_name='accommodation_elements')
     city = db.StringProperty(
         required=True, 
         verbose_name='City',
@@ -22,33 +37,42 @@ class BookingRequest(db.Model):
         required=True, verbose_name='Number of nights')
     quantity = db.IntegerProperty(
         required=True, verbose_name='Number of people')
-    email = db.EmailProperty(verbose_name='Email Address')
-    detail = db.TextProperty(verbose_name='Request Detail')
-    state = db.StringProperty(
-        default='temporary', choices=['temporary', 'confirmed'])
+    xmlSource = db.TextProperty(verbose_name='Source Detail')
 
     def listing_name(self):
-        return '%s' % self.referenceNumber
+        return '%s' % self.city
 
     def rdelete(self):
         self.delete()
 
+class GuestElement(db.Model):
+    created = db.DateTimeProperty(auto_now_add=True)
+    creator = db.UserProperty()
+    enquiry = db.ReferenceProperty(
+        Enquiry, collection_name='guest_elements')
+    isPrimary = db.BooleanProperty(default=True, verbose_name="Primary Guest")
+    surname = db.StringProperty(required=True)
+    firstNames = db.StringProperty(required=True, verbose_name="First Names")
+    email = db.EmailProperty(verbose_name='Email Address')
+    contactNumber = db.PhoneNumberProperty(verbose_name='Contact Number')
+    identifyingNumber = db.StringProperty(verbose_name='Identifying Number')
+    xmlSource = db.TextProperty(verbose_name='Source Detail')
         
 class ContractedBooking(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     creator = db.UserProperty()
     bookingNumber = db.StringProperty(required=True)
     client = db.ReferenceProperty(
-        Client, collection_name='client_contracted_bookings')
+        Client, collection_name='contraced_bookings')
     slot = db.ReferenceProperty(
-        Slot, collection_name='slot_contracted_bookings')
-    request = db.ReferenceProperty(
-        BookingRequest, collection_name='request_bookings')
+        Slot, collection_name='contracted_bookings')
+    enquiry = db.ReferenceProperty(
+        Enquiry, collection_name='contracted_bookings')
     state = db.StringProperty(
         default='temporary', choices=['temporary', 'confirmed'])
 
     def listing_name(self):
-        return '%s' % self.referenceNumber
+        return '%s' % self.bookingNumber
 
     def rdelete(self):
         self.delete()
