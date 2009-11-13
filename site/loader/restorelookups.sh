97c1f62309a843544fb6bba75d7cd581a217b024
jurgen.blignaut <jurgen.blignaut@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-13 17:06:43 +0000

    Exported lookups data; created dedicated restorelookups.sh script

--- site/loader/restorelookups.sh
+++ site/loader/restorelookups.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-APP_DIR=$1
-DATA_DIR=$2
-HOST=$3
-bulkloader.py --restore --app_id=bookings-dev --email=jurgen.blignaut@gmail.com --kind=CodeLookup --url=$HOST/unload --filename=$DATA_DIR/lookups $APP_DIR
+HOST=$1
+bulkloader.py --restore --app_id=bookings-dev --email=jurgen.blignaut@gmail.com --kind=CodeLookup --url=$HOST/unload --filename=loader/data/lookups .
 rm -rf bulkloader-*
