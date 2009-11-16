91918e175a6cefc5f7e43b48ce79a56b2038c09f
jurgen.blignaut <jurgen.blignaut@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-16 12:54:55 +0000

    Added teams playing each other per match; refined display of matches

--- trunk/site/models/schedule.py
+++ trunk/site/models/schedule.py
@@ -1,5 +1,7 @@
 from google.appengine.ext import db
 
+from models.codelookup import getChoices
+
 class Match(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     creator = db.UserProperty()
@@ -8,13 +10,15 @@ class Match(db.Model):
     date = db.DateTimeProperty(
         required=True, verbose_name='Match Date and Time')
     city = db.StringProperty(
-        required=True, verbose_name='City')
+        required=True, verbose_name='City', choices=getChoices('CTY'))
     stadium = db.StringProperty(
         required=True, verbose_name='Stadium')
+    groupTeams = db.StringProperty(
+        verbose_name='Group Teams in Match')
 
     def listing_name(self):
-        return "%s, %s, %s, %s" %  \
-          (self.number, self.date, self.city, self.stadium)
+        return "%s, %s, %s, %s, %s" %  \
+          (self.number, self.date, self.city, self.stadium, self.groupTeams)
 
     def rdelete(self):
         self.delete()
