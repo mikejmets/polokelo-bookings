3ee528ac0ca6a9d3f05ba01b39b237240a7df820
jurgen.blignaut <jurgen.blignaut@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-17 10:24:52 +0000

    Use ElementTree to add sub elements to original xml structure, and return the enhanced structure

--- site/externalrequests/externalhandlers.py
+++ site/externalrequests/externalhandlers.py
@@ -8,48 +8,34 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import datastore_errors
 from google.appengine.api import users
 
-from xml.etree.ElementTree import XML
+from xml.etree.ElementTree import XML, SubElement, tostring
 
-from models.bookinginfo import BookingRequest
+# from models.bookinginfo import ContractedBooking
 from controllers.bookingstool import BookingsTool
 
 
 class ExternalBookings(webapp.RequestHandler):
 
-    def _getAvailabilityCheckData(self, booking):
-        result = {}
-        result['bookingNumber'] = booking.findtext('bookingnumber')
-        result['city'] = booking.findtext('city')
-        result['accommodationtype'] = booking.findtext('accommodationtype')
-        result['bookingdate'] = datetime.strptime(booking.findtext('bookingdate'), '%Y-%m-%d').date()
-        result['duration'] = int(booking.findtext('duration'))
-        result['numadults'] = int(booking.findtext('numadults'))
-        result['numchildren'] = int(booking.findtext('numchildren'))
-        result['numinfants'] = int(booking.findtext('numinfants'))
-        logging.info(result)
-        return result
-
     def _checkAvailability(self, node):
         bookings = node.findall('booking')
         result = ""
+        bt = BookingsTool()
+
         for booking in bookings:
-            detail = self._getAvailabilityCheckData(booking)
-
-            bt = BookingsTool()
             available = bt.checkAvailability( \
-                    detail['city'], 
-                    detail['accommodationtype'],
-                    detail['bookingdate'],
-                    detail['duration'],
-                    detail['numadults'] + detail['numchildren'] + detail['numinfants']) and 'available' or 'not available'
+                booking.findtext('city'), 
+                booking.findtext('accommodationtype'),
+                datetime.strptime(booking.findtext('bookingdate'), '%Y-%m-%d').date(),
+                int(booking.findtext('duration')),
+                int(booking.findtext('numadults')) + \
+                        int(booking.findtext('numchildren')) + \
+                        int(booking.findtext('numinfants'))) \
+                                and 'available' or 'not available'
 
-            result += """<booking>
-<bookingnumber>%s</bookingnumber>
-<result>%s</result>
-</booking>
-""" % (detail['bookingNumber'], available)
+            avail_elem = SubElement(booking, 'availability')
+            avail_elem.text = available
 
-        return result
+        return tostring(node)
 
     def post(self):
         xml = self.request.body
@@ -61,14 +47,13 @@ class ExternalBookings(webapp.RequestHandler):
         if action.lower() == 'check availability':
             result = self._checkAvailability(xmlroot)
         else:
-            result = "<error>Unable to determine what to do</error>"
+            error_elem = SubElement(xmlroot, 'error')
+            error_elem.text = 'Unable to determine what to do'
+            result = tostring(xmlroot)
  
         # return the result
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('<bookingrequest>\n')
-        self.response.out.write('<action>%s</action>\n' % action)
         self.response.out.write(result)
-        self.response.out.write('</bookingrequest>\n')
 
 
 application = webapp.WSGIApplication([
