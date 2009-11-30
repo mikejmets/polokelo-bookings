bb2314fac5a3a9d24eea2f9df7069e134b6d0dc6
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-11-30 09:44:59 +0000

    hide transition for onhold enq

--- site/controllers/enquiry.py
+++ site/controllers/enquiry.py
@@ -39,7 +39,7 @@ class ViewEnquiry(webapp.RequestHandler):
         show_search = enquiry.workflowState in \
             ['temporary', 'onhold', 'requiresintervention']
         show_transitions = enquiry.workflowState not in \
-            ['temporary', 'requiresintervention', 'expired', 'cancelled']
+            ['temporary', 'onhold', 'requiresintervention', 'expired', 'cancelled']
         transitions = None
         if show_transitions:
             transitions = enquiry.getPossibleTransitions()
