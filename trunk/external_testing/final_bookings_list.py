9fb287517cb7b57919fe4bfc256bf2cc2b70b715
jurgen.blignaut <jurgen.blignaut@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-12-01 09:08:10 +0000

    Set up two enquiries to go into the same batch, one auto, the other manual. Ready to test payment processing

--- trunk/external_testing/final_bookings_list.py
+++ trunk/external_testing/final_bookings_list.py
@@ -7,7 +7,7 @@ url = 'http://localhost:8080/externalbookings'
 
 xml = """
 <enquirylist>
-    <enquirybatchnumber>YHT-4W7-9ZJ</enquirybatchnumber>
+    <enquirybatchnumber>YHT-4W7-9ZF</enquirybatchnumber>
     <guestagentcode>Trafalgar-234234324</guestagentcode>
     <action>confirm enquiries</action>
     <creditcardholder>
@@ -29,7 +29,7 @@ xml = """
     </creditcardholder>
     <enquiries>
         <enquiry>
-            <enquirynumber>YK8-DQN-2P8</enquirynumber>
+            <enquirynumber>YK8-DQN-2PH</enquirynumber>
             <city>CPT</city>
             <accommodation>
                 <type>GH</type>
@@ -40,7 +40,7 @@ xml = """
                     <family>0</family>
                 </rooms>
             </accommodation>
-            <startdate>2010-06-17</startdate>
+            <startdate>2010-06-20</startdate>
             <duration>4</duration>
             <adults>2</adults>
             <children>2</children>
@@ -50,6 +50,29 @@ xml = """
             </disability>
             <quote>27680.0</quote>
             <vat>3875.2</vat>
+        </enquiry>
+        <enquiry>
+            <enquirynumber>YK8-DQN-2PJ</enquirynumber>
+            <city>PCS</city>
+            <accommodation>
+                <type>GH</type>
+                <rooms>
+                    <single>2</single>
+                    <twin>0</twin>
+                    <double>1</double>
+                    <family>0</family>
+                </rooms>
+            </accommodation>
+            <startdate>2010-06-25</startdate>
+            <duration>4</duration>
+            <adults>2</adults>
+            <children>2</children>
+            <disability>
+                <wheelchairaccess>no</wheelchairaccess>
+                <otherspecialneeds>no</otherspecialneeds>
+            </disability>
+            <quote>0.0</quote>
+            <vat>0.0</vat>
         </enquiry>
     </enquiries>
 </enquirylist>
