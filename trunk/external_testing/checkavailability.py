b16cb83729a21a70ce3dee4d77b0ba4857a563f3
jurgen.blignaut <jurgen.blignaut@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-12-02 19:44:40 +0000

    Created data snapshots and test scripts to test external interface

--- trunk/external_testing/checkavailability.py
+++ trunk/external_testing/checkavailability.py
@@ -5,21 +5,17 @@ url = 'http://localhost:8080/externalbookings'
 # url = 'http://bookings-dev.appspot.com/externalbookings'
 
 
-# # get a new collection number
-# xml = """
-# <testgenerator>
-#     <action>generate collection number</action>
-# </testgenerator>
-# """
-# 
-# req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
-# response = urllib2.urlopen(req)
-# result = response.read()
-# 
-# xmlroot = XML(result)
-# collection_number = xmlroot.findtext('collectionnumber')
+# get a new collection number
+xml = """
+<testgenerator>
+    <action>generate collection number</action>
+</testgenerator>
+"""
 
-collection_number = 'YHT-4W7-9ZJ'
+req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
+response = urllib2.urlopen(req)
+xmlroot = XML(response.read())
+collection_number = xmlroot.findtext('collectionnumber')
 
 
 # get a new enquiry number
@@ -31,9 +27,7 @@ xml = """
 
 req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
 response = urllib2.urlopen(req)
-result = response.read()
-
-xmlroot = XML(result)
+xmlroot = XML(response.read())
 enquiry_number = xmlroot.findtext('enquirynumber')
 
 
@@ -55,8 +49,8 @@ xml = """
             <family>0</family>
         </rooms>
     </accommodation>
-    <startdate>2010-07-10</startdate>
-    <duration>3</duration>
+    <startdate>2010-06-10</startdate>
+    <duration>5</duration>
     <adults>2</adults>
     <children>2</children>
     <disability>
@@ -66,6 +60,99 @@ xml = """
 </enquiry>
 """ % (collection_number, enquiry_number)
 
+req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
+response = urllib2.urlopen(req)
+print response.read()
+
+
+# get a new enquiry number
+xml = """
+<testgenerator>
+    <action>generate enquiry number</action>
+</testgenerator>
+"""
+
+req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
+response = urllib2.urlopen(req)
+xmlroot = XML(response.read())
+enquiry_number = xmlroot.findtext('enquirynumber')
+
+
+# post the check availability request
+xml = """
+<enquiry>
+    <enquirybatchnumber>%s</enquirybatchnumber>
+    <email>john@ogroats.co.uk</email>
+    <guestagentcode>Trafalgar-234234234</guestagentcode>
+    <action>check availability</action>
+    <enquirynumber>%s</enquirynumber>
+    <city>DUR</city>
+    <accommodation>
+        <type>GH</type>
+        <rooms>
+            <single>2</single>
+            <twin>0</twin>
+            <double>1</double>
+            <family>0</family>
+        </rooms>
+    </accommodation>
+    <startdate>2010-06-17</startdate>
+    <duration>5</duration>
+    <adults>2</adults>
+    <children>2</children>
+    <disability>
+        <wheelchairaccess>no</wheelchairaccess>
+        <otherspecialneeds>no</otherspecialneeds>
+    </disability>
+</enquiry>
+""" % (collection_number, enquiry_number)
+
+req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
+response = urllib2.urlopen(req)
+print response.read()
+
+
+# get a new enquiry number
+xml = """
+<testgenerator>
+    <action>generate enquiry number</action>
+</testgenerator>
+"""
+
+req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
+response = urllib2.urlopen(req)
+xmlroot = XML(response.read())
+enquiry_number = xmlroot.findtext('enquirynumber')
+
+
+# post the check availability request
+xml = """
+<enquiry>
+    <enquirybatchnumber>%s</enquirybatchnumber>
+    <email>john@ogroats.co.uk</email>
+    <guestagentcode>Trafalgar-234234234</guestagentcode>
+    <action>check availability</action>
+    <enquirynumber>%s</enquirynumber>
+    <city>CPT</city>
+    <accommodation>
+        <type>GH</type>
+        <rooms>
+            <single>2</single>
+            <twin>0</twin>
+            <double>1</double>
+            <family>0</family>
+        </rooms>
+    </accommodation>
+    <startdate>2010-06-25</startdate>
+    <duration>3</duration>
+    <adults>2</adults>
+    <children>2</children>
+    <disability>
+        <wheelchairaccess>no</wheelchairaccess>
+        <otherspecialneeds>no</otherspecialneeds>
+    </disability>
+</enquiry>
+""" % (collection_number, enquiry_number)
 
 req = urllib2.Request(url, xml, headers={'Content-Type':'text/plain'})
 response = urllib2.urlopen(req)
