0d56d81475d0f36d40d899044283c47b24980469
jurgen.blignaut <jurgen.blignaut@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-12-07 14:22:58 +0000

    Added refund tests

---
+++ external_testing/prod/refund.py
@@ -0,0 +1,35 @@
+import urllib, urllib2
+from xml.etree.ElementTree import XML, SubElement, tostring
+
+url = 'http://0-14-alpha.latest.bookings-dev.appspot.com/paymentnotify'
+
+pay_req = {}
+pay_req["timestamp"] = '2009-12-03 10:00'
+pay_req["p1"] = '1364'
+pay_req["p2"] = 'YCHT-4W7-9Z2'
+pay_req["p3"] = '123456APPROVED'
+pay_req["p4"] = ""
+pay_req["p5"] = "J O'GROATS"
+pay_req["p6"] = "-6311.04" 
+pay_req["p7"] = "VISA"
+pay_req["p8"] = "Polokelo Sport Tours Accommodation: YHT-4W7-9ZJ Refund"
+pay_req["p9"] = "jurgen@upfrontsystems.co.za"
+pay_req["p10"] = "00"
+pay_req["p11"] = "1105"
+pay_req["p12"] = "00"
+# pay_req["p12"] = "05"
+pay_req["pam"] = ""
+pay_req["m_1"] = "YCHT-4W7-9Z2"
+pay_req["m_2"] = "Refund after deposit was paid twice"
+pay_req["m_3"] = "REFUND"
+pay_req["m_4"] = ""
+pay_req["CardHolderIpAddr"] = "10.0.0.128"
+pay_req["MaskedCardNumber"] = "************1234"
+pay_req["TransactionType"] = "Authorisation"
+
+payment = urllib.urlencode(pay_req)
+
+req = urllib2.Request(url, payment)
+response = urllib2.urlopen(req)
+result = response.read()
+print result
