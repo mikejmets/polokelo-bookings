3f4ca56c19459e8eca3cd8de44f991e4cc2aa303
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-12-11 10:42:13 +0000

    Set defaults

--- scripts/update_datastore.py
+++ scripts/update_datastore.py
@@ -3,9 +3,9 @@ import urllib2
 
 
 def main(): 
-  key = '2009-12-10 03:34:34.597862'
+  key = 'None'
   url = 'http://www.polokelo-bookings.co.za/tasks/update_datastore'
-  #url = 'http://localhost:8080/tasks/update_datastore'
+  url = 'http://localhost:8080/tasks/update_datastore'
   cnt = 0
   while True:
       data = urllib.urlencode({'last_key':key})
