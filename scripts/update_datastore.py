d0e690f0138c69c613d71f80290ac5eff1d1a518
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-12-11 10:16:27 +0000

    Added try except

--- scripts/update_datastore.py
+++ scripts/update_datastore.py
@@ -2,23 +2,25 @@ import urllib
 import urllib2
 
 
-def main():
-  data = urllib.urlencode({'last_key':'None'})
+def main(): 
+  key = '2009-12-10 03:34:34.597862'
   url = 'http://www.polokelo-bookings.co.za/tasks/update_datastore'
   #url = 'http://localhost:8080/tasks/update_datastore'
   cnt = 0
   while True:
-      next_url = url + '?' + data
-      response = urllib2.urlopen(next_url)
-      results = response.read()
-      #print '--------%s' % results
-      results = eval(results)
-      next_url = results['next_url']
-      if next_url == '/':
-          break
-      #url = next_url.split('?')[0]
-      key = next_url.split('=')[1]
       data = urllib.urlencode({'last_key':key})
+      next_url = url + '?' + data
+      try:
+          response = urllib2.urlopen(next_url)
+          results = response.read()
+          #print '--------%s' % results
+          results = eval(results)
+          next_url = results['next_url']
+          if next_url == '/':
+              break
+          key = next_url.split('=')[1]
+      except:
+          print 'Reset key -------- %s: %s' % (cnt, key)
       
       print '-- %s: %s' % (cnt, key)
       cnt += 1
