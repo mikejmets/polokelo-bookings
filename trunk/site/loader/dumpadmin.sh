525718c58436d611897e5c9c24f299f1858e9dad
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2009-12-22 20:07:35 +0000

    Added restorebackup and change all restore to take app_id arg

--- trunk/site/loader/dumpadmin.sh
+++ trunk/site/loader/dumpadmin.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 HOST=$1
 EMAIL=$2
+APP_ID=polokelo-bookings
 echo "-------------- Dump Lookup Tables"
 rm -rf loader/data/lookups
-bulkloader.py --dump --num_threads=1 --app_id=polokelo-bookings --email=$EMAIL --kind=CodeLookup --url=$HOST/unload --filename=loader/data/lookups .
+bulkloader.py --dump --num_threads=1 --app_id=$APP_ID --email=$EMAIL --kind=CodeLookup --url=$HOST/unload --filename=loader/data/lookups .
 echo "-------------- Dump Packages"
 rm -rf loader/data/packages
-bulkloader.py --dump --num_threads=1 --app_id=polokelo-bookings --email=$EMAIL --kind=Package --url=$HOST/unload --filename=loader/data/packages .
+bulkloader.py --dump --num_threads=1 --app_id=$APP_ID --email=$EMAIL --kind=Package --url=$HOST/unload --filename=loader/data/packages .
 echo "-------------- Dump Match Schedules"
 rm -rf loader/data/matches
-bulkloader.py --dump --num_threads=1 --app_id=polokelo-bookings --email=$EMAIL --kind=Match --url=$HOST/unload --filename=loader/data/matches .
+bulkloader.py --dump --num_threads=1 --app_id=$APP_ID --email=$EMAIL --kind=Match --url=$HOST/unload --filename=loader/data/matches .
+echo "-------------- Dump userRoles"
+rm -rf loader/data/userroles
+bulkloader.py --dump --num_threads=1 --app_id=$APP_ID --email=$EMAIL --kind=UserRole --url=$HOST/unload --filename=loader/data/userroles .
 rm -rf bulkloader-*
