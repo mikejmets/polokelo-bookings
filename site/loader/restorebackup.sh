5f7b522909c9df95dd431634a8fc31e50ea4e2a4
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2010-01-11 19:52:27 +0000

    Made an attempt to get backup/restore working - no joy

--- site/loader/restorebackup.sh
+++ site/loader/restorebackup.sh
@@ -3,6 +3,6 @@ EMAIL=$1
 HOST=http://backup-pb.appspot.com
 APP_ID=backup-pb
 
-loader/restoreadmin.sh $HOST $EMAIL $APP_ID
+#loader/restoreadmin.sh $HOST $EMAIL $APP_ID
 loader/restorehosts.sh $HOST $EMAIL $APP_ID
 
