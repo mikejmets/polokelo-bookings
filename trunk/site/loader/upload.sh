23957132e75be770bc0d032b1fc177e8dd773a38
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2010-01-11 19:52:27 +0000

    Made an attempt to get backup/restore working - no joy

--- trunk/site/loader/upload.sh
+++ trunk/site/loader/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SRC=$1
-DIR=$2
-appcfg.py upload_data --kind=Owner --filename=$SRC/owners.csv --config_file=loader/owner_loader.py --email=mikejmets@gmail.com -v --url=http://localhost:8080/unload $DIR
-#appcfg.py upload_data --kind=Venue --filename=$SRC/venues.csv --config_file=loader/venue_loader.py --email=mikejmets@gmail.com -v --url=http://localhost:8080/unload $DIR
+DEST=$2
+DIR=$3
+EMAIL=$4
+appcfg.py upload_data --kind=Owner --filename=$SRC/owners.csv --config_file=loader/owner_loader.py --email=$EMAIL -v --url=$DEST/unload $DIR
+#appcfg.py upload_data --kind=Venue --filename=$SRC/venues.csv --config_file=loader/venue_loader.py --email=$EMAIL -v --url=$DEST/unload $DIR
 
