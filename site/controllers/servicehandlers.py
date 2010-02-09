c9dfb24c1c38b167a5a041db9b1c8be432aa3e50
mikejmets <mikejmets@25470cc2-c7b8-11de-9d4f-f947ee5921c8>
2010-02-09 09:51:04 +0000

    Added basic service and provider CRUD

--- site/controllers/servicehandlers.py
+++ site/controllers/servicehandlers.py
@@ -7,6 +7,10 @@ from google.appengine.api import users
 
 from controllers.home import BASE_PATH, PROJECT_PATH
 from controllers.address import CaptureAddress, EditAddress, DeleteAddress
+from controllers.provider import ManageProviders, \
+    ViewProvider, CaptureProvider, EditProvider, DeleteProvider
+from controllers.service import ViewService, \
+    CaptureService, EditService, DeleteService
 from controllers.owner import ViewOwner, CaptureOwner, EditOwner, DeleteOwner
 from controllers.venue import ViewVenue, CaptureVenue, EditVenue, DeleteVenue
 from controllers.bedroom \
@@ -51,6 +55,21 @@ from controllers.utils import get_authentication_urls
 #    roles=['admin'])
 
 PAGESIZE = 15
+class ManageServices(webapp.RequestHandler):
+    def get(self):
+
+        auth_url, auth_url_text = get_authentication_urls(self.request.uri)
+
+        filepath = os.path.join(PROJECT_PATH, 'templates', 'services', 'manageservices.html')
+        self.response.out.write(template.render(filepath, 
+          {
+              'base_path':BASE_PATH,
+              'user':users.get_current_user(),
+              'is_admin_user':users.is_current_user_admin(),
+              'auth_url':auth_url,
+              'auth_url_text':auth_url_text
+              }))
+
 class ManageHosts(webapp.RequestHandler):
     def get(self):
         #acl = Acl(area='hostinfo',
@@ -79,56 +98,75 @@ class ManageHosts(webapp.RequestHandler):
               }))
 
 application = webapp.WSGIApplication([
-                  ('/services/hostinfo', ManageHosts),
-                  ('/services/owner/viewowner', ViewOwner),
-                  ('/services/owner/captureowner', CaptureOwner),
-                  ('/services/owner/editowner', EditOwner),
-                  ('/services/owner/deleteowner', DeleteOwner),
-                  ('/services/owner/address/captureaddress', CaptureAddress),
-                  ('/services/owner/address/editaddress', EditAddress),
-                  ('/services/owner/address/deleteaddress', DeleteAddress),
-                  ('/services/owner/contact/capturephone', CapturePhoneNumber),
-                  ('/services/owner/contact/editphone', EditPhoneNumber),
-                  ('/services/owner/contact/deletephone', DeletePhoneNumber),
-                  ('/services/owner/email/captureemail', CaptureEmailAddress),
-                  ('/services/owner/email/editemail', EditEmailAddress),
-                  ('/services/owner/email/deleteemail', DeleteEmailAddress),
-                  ('/services/owner/viewvenue', ViewVenue),
-                  ('/services/owner/capturevenue', CaptureVenue),
-                  ('/services/owner/editvenue', EditVenue),
-                  ('/services/owner/deletevenue', DeleteVenue),
-                  ('/services/owner/venue/address/captureaddress', CaptureAddress),
-                  ('/services/owner/venue/address/editaddress', EditAddress),
-                  ('/services/owner/venue/address/deleteaddress', DeleteAddress),
-                  ('/services/owner/venue/contact/capturephone', CapturePhoneNumber),
-                  ('/services/owner/venue/contact/editphone', EditPhoneNumber),
-                  ('/services/owner/venue/contact/deletephone', DeletePhoneNumber),
-                  ('/services/owner/venue/email/captureemail', CaptureEmailAddress),
-                  ('/services/owner/venue/email/editemail', EditEmailAddress),
-                  ('/services/owner/venue/email/deleteemail', DeleteEmailAddress),
-                  ('/services/owner/venue/capturephotograph', CapturePhotograph),
-                  ('/services/owner/venue/editphotograph', EditPhotograph),
-                  ('/services/owner/venue/deletephotograph', DeletePhotograph),
-                  ('/services/owner/venue/viewbedroom', ViewBedroom),
-                  ('/services/owner/venue/capturebedroom', CaptureBedroom),
-                  ('/services/owner/venue/editbedroom', EditBedroom),
-                  ('/services/owner/venue/deletebedroom', DeleteBedroom),
-                  ('/services/owner/venue/capturebed', CaptureBed),
-                  ('/services/owner/venue/editbed', EditBed),
-                  ('/services/owner/venue/deletebed', DeleteBed),
-                  ('/services/owner/venue/capturebathroom', CaptureBathroom),
-                  ('/services/owner/venue/editbathroom', EditBathroom),
-                  ('/services/owner/venue/deletebathroom', DeleteBathroom),
-                  ('/services/owner/venue/captureinspection', CaptureInspection),
-                  ('/services/owner/venue/editinspection', EditInspection),
-                  ('/services/owner/venue/deleteinspection', DeleteInspection),
-                  ('/services/owner/venue/capturecomplaint', CaptureComplaint),
-                  ('/services/owner/venue/editcomplaint', EditComplaint),
-                  ('/services/owner/venue/deletecomplaint', DeleteComplaint),
-                  ('/services/owner/venue/viewbed', ViewBed),
-                  ('/services/owner/venue/viewberth', ViewBerth),
-                  ('/services/owner/venue/viewslot', ViewSlot),
-                  ], debug=True)
+                ('/services', ManageServices),
+                ('/services/providers', ManageProviders),
+                ('/services/provider/viewprovider', ViewProvider),
+                ('/services/provider/captureprovider', CaptureProvider),
+                ('/services/provider/editprovider', EditProvider),
+                ('/services/provider/deleteprovider', DeleteProvider),
+                ('/services/provider/address/captureaddress', CaptureAddress),
+                ('/services/provider/address/editaddress', EditAddress),
+                ('/services/provider/address/deleteaddress', DeleteAddress),
+                ('/services/provider/contact/capturephone', CapturePhoneNumber),
+                ('/services/provider/contact/editphone', EditPhoneNumber),
+                ('/services/provider/contact/deletephone', DeletePhoneNumber),
+                ('/services/provider/email/captureemail', CaptureEmailAddress),
+                ('/services/provider/email/editemail', EditEmailAddress),
+                ('/services/provider/email/deleteemail', DeleteEmailAddress),
+                ('/services/provider/viewservice', ViewService),
+                ('/services/provider/captureservice', CaptureService),
+                ('/services/provider/editservice', EditService),
+                ('/services/provider/deleteservice', DeleteService),
+                ('/services/hostinfo', ManageHosts),
+                ('/services/owner/viewowner', ViewOwner),
+                ('/services/owner/captureowner', CaptureOwner),
+                ('/services/owner/editowner', EditOwner),
+                ('/services/owner/deleteowner', DeleteOwner),
+                ('/services/owner/address/captureaddress', CaptureAddress),
+                ('/services/owner/address/editaddress', EditAddress),
+                ('/services/owner/address/deleteaddress', DeleteAddress),
+                ('/services/owner/contact/capturephone', CapturePhoneNumber),
+                ('/services/owner/contact/editphone', EditPhoneNumber),
+                ('/services/owner/contact/deletephone', DeletePhoneNumber),
+                ('/services/owner/email/captureemail', CaptureEmailAddress),
+                ('/services/owner/email/editemail', EditEmailAddress),
+                ('/services/owner/email/deleteemail', DeleteEmailAddress),
+                ('/services/owner/viewvenue', ViewVenue),
+                ('/services/owner/capturevenue', CaptureVenue),
+                ('/services/owner/editvenue', EditVenue),
+                ('/services/owner/deletevenue', DeleteVenue),
+                ('/services/owner/venue/address/captureaddress', CaptureAddress),
+                ('/services/owner/venue/address/editaddress', EditAddress),
+                ('/services/owner/venue/address/deleteaddress', DeleteAddress),
+                ('/services/owner/venue/contact/capturephone', CapturePhoneNumber),
+                ('/services/owner/venue/contact/editphone', EditPhoneNumber),
+                ('/services/owner/venue/contact/deletephone', DeletePhoneNumber),
+                ('/services/owner/venue/email/captureemail', CaptureEmailAddress),
+                ('/services/owner/venue/email/editemail', EditEmailAddress),
+                ('/services/owner/venue/email/deleteemail', DeleteEmailAddress),
+                ('/services/owner/venue/capturephotograph', CapturePhotograph),
+                ('/services/owner/venue/editphotograph', EditPhotograph),
+                ('/services/owner/venue/deletephotograph', DeletePhotograph),
+                ('/services/owner/venue/viewbedroom', ViewBedroom),
+                ('/services/owner/venue/capturebedroom', CaptureBedroom),
+                ('/services/owner/venue/editbedroom', EditBedroom),
+                ('/services/owner/venue/deletebedroom', DeleteBedroom),
+                ('/services/owner/venue/capturebed', CaptureBed),
+                ('/services/owner/venue/editbed', EditBed),
+                ('/services/owner/venue/deletebed', DeleteBed),
+                ('/services/owner/venue/capturebathroom', CaptureBathroom),
+                ('/services/owner/venue/editbathroom', EditBathroom),
+                ('/services/owner/venue/deletebathroom', DeleteBathroom),
+                ('/services/owner/venue/captureinspection', CaptureInspection),
+                ('/services/owner/venue/editinspection', EditInspection),
+                ('/services/owner/venue/deleteinspection', DeleteInspection),
+                ('/services/owner/venue/capturecomplaint', CaptureComplaint),
+                ('/services/owner/venue/editcomplaint', EditComplaint),
+                ('/services/owner/venue/deletecomplaint', DeleteComplaint),
+                ('/services/owner/venue/viewbed', ViewBed),
+                ('/services/owner/venue/viewberth', ViewBerth),
+                ('/services/owner/venue/viewslot', ViewSlot),
+                ], debug=True)
 
 
 def main():
